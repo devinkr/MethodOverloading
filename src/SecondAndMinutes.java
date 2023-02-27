e255ded3edbb3d05dbe1172a6946c7a432351650
Devin Raleigh <devinkr@gmail.com>
2023-02-27 14:00:00 -0700

    Refactor getDuration. Add test cases.

--- src/SecondAndMinutes.java
+++ src/SecondAndMinutes.java
@@ -1,23 +1,26 @@
 public class SecondAndMinutes {
 
     public static void main(String[] args) {
-        System.out.printf(getDurationString(5000));
+        System.out.println(getDurationString(3945));
+        System.out.println(getDurationString(65, 45));
+        System.out.println(getDurationString(-10));
+        System.out.println(getDurationString(-10, 20));
+        System.out.println(getDurationString(65, 61));
+        System.out.println(getDurationString(65, -10));
+        System.out.println(getDurationString(65, 60));
+
+
     }
 
     public static String getDurationString(int seconds) {
-        int finalHours = 0;
-        int finalMinutes = 0;
-        int finalSeconds = seconds;
-        if (finalSeconds < 0) return "Invalid Input: Seconds must be >= 0";
-        if (finalSeconds >= 3600) {
-            finalHours = finalSeconds / 3600;
-            finalSeconds = finalSeconds % 3600;
-        }
-        if (finalSeconds >= 60) {
-            finalMinutes = finalSeconds / 60;
-            finalSeconds = finalSeconds % 60;
-        }
-        return finalHours + "h " + finalMinutes + "m " + finalSeconds + "s";
+        if (seconds < 0) return "Invalid Input: Seconds must be >= 0";
+
+        int minutes = seconds / 60;
+        int hours = minutes / 60;
+        int remainingMinutes = minutes % 60;
+        int remainingSeconds = seconds % 60;
+
+        return hours + "h " + remainingMinutes + "m " + remainingSeconds + "s";
     }
 
     public static String getDurationString(int minutes, int seconds) {
